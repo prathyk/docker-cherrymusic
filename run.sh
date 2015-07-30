47550c8fa22df49ee33a9059e47c28b516b6dc3f
Andreas Linz <klingt.net@gmail.com>
2015-07-30 15:43:30 +0200

    shell script to run the container
    - later on you can use the systemd service to run it

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+NAME=${cm:-alpine}
+HTTP_PORT=${1:-9090}
+MUSIC_PATH=${2:-$HOME/Music}
+
+docker run \
+	--name ${NAME} \
+	--publish ${HTTP_PORT}:8080 \
+	--volumes-from cm-alpine-data \
+	--volume ${MUSIC_PATH}:/home/cm/Music \
+	-it cm-alpine
